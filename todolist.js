ed4829e06e625a60075119c7f679730a351e3f25
Henrique <coelhovsk2007@gmail.com>
2025-06-16 09:59:03 -0300

    tentando desbufar essa porr

--- todolist.js
+++ todolist.js
@@ -2,6 +2,7 @@ document.getElementById('addTaskBtn').addEventListener('click', function () {
     const taskInput = document.getElementById('taskInput');
     const taskText = taskInput.value.trim();
 
+    
     if (taskText !== '') {
         const taskList = document.getElementById('taskList');
         const newTask = document.createElement('li');
